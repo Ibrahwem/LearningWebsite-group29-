52a173a2a776656578c53c5ab1977f1dafbd94c7
Ibrahem Wattaweda <wattad111@gmail.com>
2020-01-09 23:23:15 +0200

    Adding a unit test file to project

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LearningWebsite_Group29_;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
